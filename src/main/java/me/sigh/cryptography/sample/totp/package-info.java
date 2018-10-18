73a23a2dee63b9ee35e8fb4a46cfc0b6958e9755
wangtingbang <wangtingbang@gmail.com>
2018-10-18 17:39:32 +0800

    add totp package

---
+++ src/main/java/me/sigh/cryptography/sample/totp/package-info.java
@@ -0,0 +1,8 @@
+package me.sigh.cryptography.sample.totp;
+
+
+/**
+ * 结合google-authenticator
+ * 基于TOTP：Time-based One-time Password Algorithm（基于时间的一次性密码算法）
+ * TODO: ref https://github.com/google/google-authenticator-android/tree/master/AuthenticatorApp/src/main/java/com/google/android/apps/authenticator
+ */
